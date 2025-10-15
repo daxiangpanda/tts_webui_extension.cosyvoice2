124fe8e30a8bd8538b5d5b04f833782cf0d44e7b
lxz <daxiangpanda@users.noreply.github.com>
2025-10-15 17:35:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     scripts=[],
     install_requires=[
         "gradio",
-        "cosyvoice @ git+https://github.com/rsxdalv/CosyVoice@main",
+        "cosyvoice @ git+https://github.com/FunAudioLLM/CosyVoice@main",
         "modelscope>=1.25.0",
     ],
     classifiers=[
@@ -20,3 +20,4 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
 )
+
